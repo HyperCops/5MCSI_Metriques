597eeb3c71939787c4762acdfc03f0a6e6030a53
HyperCops <anthonydealmeidamajor@gmail.com>
2026-01-05 16:18:40 +0100

    __init__.py

--- __init__.py
+++ __init__.py
@@ -34,6 +34,10 @@ def mongraphique():
 @app.route("/histogramme/")
 def monhistogramme():
     return render_template("histogramme.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
   
 if __name__ == "__main__":
   app.run(debug=True)
